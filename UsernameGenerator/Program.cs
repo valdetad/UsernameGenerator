d678e73141f4174303a53885851c56b18cba5b72
valdetademiri <valdetademiri2@gmail.com>
2024-03-12 21:10:49 +0100

    added a method which will let the user to type only letters.

--- UsernameGenerator/Program.cs
+++ UsernameGenerator/Program.cs
@@ -4,42 +4,78 @@ class Program
 {
     static void Main(string[] args)
     {
+   
         Console.WriteLine("Enter your name: ");
-        string name = Console.ReadLine();
+        string name = LettersOnlyInput();
 
         Console.WriteLine("Enter your surname: ");
-        string surname = Console.ReadLine();
+        string surname = LettersOnlyInput();
 
         var generatedUsername = UsernameGenerator(name, surname);
 
-
         Console.WriteLine("This is your username based on your name and surname: " + generatedUsername);
 
+        // Create separate function for this!
         Console.WriteLine("Do you like the suggested username? (Y/N)");
 
         string confirmation = Console.ReadLine();
 
-        // Check something ....
-        if (confirmation == "Y")
+        if (!string.IsNullOrWhiteSpace(confirmation))
         {
-            Console.WriteLine("Welcome to our platform, " + generatedUsername);
+            if (confirmation.ToUpper() == "Y")
+            {
+                Console.WriteLine("Welcome to our platform, " + generatedUsername);
+            }
+            else if (confirmation.ToUpper() == "N")
+            {
+                Console.WriteLine("Generating new username .... ");
+                Console.WriteLine("New username suggestion is: " + UsernameGenerator(name, surname));
+            }
+            else
+            {
+                Console.WriteLine("Invalid confiration choosen!");
+            }
         }
-        else if(confirmation == "N")
+        else
         {
-            Console.WriteLine("Here is the new username suggestion: " + );
-            
+            Console.WriteLine("Please enter valid confirmation!");
         }
+    }
 
-        // does something 
-        static string UsernameGenerator(string name, string surname)
+    static string LettersOnlyInput()
+    {
+        string input;
+        while (true)
         {
-            Random rmd = new Random();
-
-            string random = rmd.Next(1000, 9999).ToString();
-
-            string username = name + surname + random;
-
-            return username;
+            input = Console.ReadLine();
+            if (IsAllLetters(input))
+            {
+                return input;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input! Please enter only letters.");
+            }
         }
+    }
+
+    static bool IsAllLetters(string input)
+    {
+        foreach (char c in input)
+        {
+            if (!char.IsLetter(c))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static string UsernameGenerator(string name, string surname)
+    {
+        Random rmd = new Random();
+        string random = rmd.Next(1000, 9999).ToString();
+        string username = name + surname + random;
+        return username;
     }
 }
